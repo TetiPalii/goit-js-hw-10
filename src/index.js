5342401eb3cdb813eb725d3fc4a8d21512eee5b7
TetiPalii <TetianaPalii9491@gmail.com>
2023-04-21 01:09:31 +0200

    task1

--- src/index.js
+++ src/index.js
@@ -6,26 +6,48 @@ import { fetchCountries } from './fetchCountries';
 const DEBOUNCE_DELAY = 300;
 const searchInput = document.querySelector('#search-box');
 const countriesList = document.querySelector('.country-list');
+const countryContainer = document.querySelector('.country-info');
 
 searchInput.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput(e) {
   e.preventDefault();
-  if (e.target.value === '') {
+
+  if (e.target.value.trim() === '') {
+    countriesList.innerHTML = '';
+
+    return;
   }
 
   fetchCountries(e.target.value)
     .then(response => {
-      countriesList.innerHTML = createMarkup(response);
-      console.log(response);
+      if (response.length > 10) {
+        Notiflix.Notify.warning(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        countryContainer.innerHTML = '';
+        return;
+      } else if (response.length > 2 && response.length <= 10) {
+        console.log(response);
+        countryContainer.innerHTML = '';
+        const countriesNames = response
+          .map(({ name: { common }, flags: { svg } }) => {
+            return `<li class ="flag-name"><img src="${svg}" alt="${common}" width = 25 height = 15 /><h2>${common}</h2></li>`;
+          })
+          .join('');
+        countriesList.innerHTML = countriesNames;
+        return;
+      }
+      countryContainer.innerHTML = createMarkup(response);
     })
     .catch(err => {
       console.log(err);
+      Notiflix.Notify.failure('Oops, there is no country with that name');
     });
 }
 
-function createMarkup(arr) {
-  return arr
+function createMarkup(countries) {
+  return countries
     .map(
       ({
         name: { official },
@@ -34,16 +56,11 @@ function createMarkup(arr) {
         flags: { svg },
         capital,
       }) => {
-        console.log(languages);
-        console.log(official);
-        return `<li>
-        <h2>${official}</h2>
-        <p>${capital}</p>
-        <p>${population}</p>
-        <p>${languages}</p>
-        <img src="${svg}" alt="${official}" />
-        
-      </li>`;
+        return `<div class = "flex-container"><img src="${svg}" alt="${official}" width = 25 height = 15 />
+        <h2>${official}</h2></div>
+        <p>capital: ${capital}</p>
+        <p>population: ${population}</p>
+        <p>languages: ${Object.values(languages)}</p>`;
       }
     )
     .join('');
